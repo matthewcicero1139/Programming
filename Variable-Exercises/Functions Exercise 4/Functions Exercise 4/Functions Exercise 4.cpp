7da12ef0e3ff91d2ad511a39da27b5ffb2b3fe57
matthewcicero1139 <matthewcicero33@gmail.com>
2016-10-06 16:31:31 -0500

    Add files via upload

---
+++ Variable-Exercises/Functions Exercise 4/Functions Exercise 4/Functions Exercise 4.cpp
@@ -0,0 +1,23 @@
+/*
+The following statement calls a function named Half. The Half function returns a value that is
+half that of the argument. Write the function.
+
+float number = 16.721f;
+float result = Half(number); //result is 8.3605
+*/
+// need syntactically correct code
+
+#include <iostream>
+//need function definition
+float Half(float n);float number = 16.721f;
+float result = Half(number);
+
+int main()
+{
+
+}
+
+float Half(float n)
+{
+	return n / 2;	
+}
