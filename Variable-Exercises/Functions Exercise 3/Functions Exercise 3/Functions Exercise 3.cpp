093f31793c5ad6f14a91b871853ba5d11802c488
matthewcicero1139 <matthewcicero33@gmail.com>
2016-10-06 17:58:48 -0500

    Add files via upload

---
+++ Variable-Exercises/Functions Exercise 3/Functions Exercise 3/Functions Exercise 3.cpp
@@ -0,0 +1,50 @@
+
+// unfinished 
+
+
+/*
+Add another function with the same name as the one you wrote in question 2, but with
+three floats instead of two.What is this language feature called ?*/#include <iostream>
+
+float v1;
+float v2;
+float v3;
+
+int main()
+{
+
+	std::cout << "Enter Value 1: ";
+	std::cin >> v1;
+	std::cout << "Enter Value 2: ";
+	std::cin >> v2;
+	std::cout << "Enter Value 3: ";
+	std::cin >> v3;
+
+	float func();
+	{
+		// the if statments are not working. what if therte were a if and
+		/*if (v1 < v2)
+		{
+			std::cout << " The smaller number is: " << ('%i', v1) << std::endl;
+		}
+		else if (v1 < v3)
+		{
+			std::cout << " The smaller number is: " << ('%i', v1) << std::endl;
+		}
+		else if (v2 < v3)
+		{
+			std::cout << " The smaller number is: " << ('%i', v2) << std::endl;
+		}
+		else
+		{
+			std::cout << " The smaller number is: " << ('%i', v3) << std::endl;
+		}*/
+
+		// switch statement
+		// loop 
+	}
+	system("pause");
+}
+// if i use loop what would i do 
+// i would loop through 
+
