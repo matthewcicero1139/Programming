ab265e7e9e2adf0ec18ff8b43f70c45162da8274
matthewcicero1139 <matthewcicero33@gmail.com>
2016-10-14 17:57:47 -0500

    Add files via upload

---
+++ Stuff-in-Class/Rock_Paper_Scissors/Rock_Paper_Scissors/Rock_Paper_Scissors.cpp
@@ -0,0 +1,27 @@
+#include "Rock_Paper_Scissors.h"
+#include <iostream>
+
+RPS::RPS()
+{
+}
+
+// function rock
+int RPS::rock()
+{
+
+	return 0;
+}
+
+// function paper
+int RPS::paper()
+{
+	return 0;
+}
+
+// function scissor
+int RPS::scissor()
+{
+	return 0;
+}
+
+//
---
+++ Stuff-in-Class/Rock_Paper_Scissors/Rock_Paper_Scissors/Rock_Paper_Scissors.h
@@ -0,0 +1,15 @@
+#pragma once
+
+class RPS
+{
+	
+public:
+	// constructors
+	RPS();
+	// functions prototypes
+	int rock();
+	int paper();
+	int scissor();
+
+};
+
