3c212581bd3720eac0893b56160d124972340ce4
matthewcicero1139 <matthewcicero33@gmail.com>
2016-10-14 17:58:17 -0500

    Add files via upload

---
+++ Stuff-in-Class/Class Practice/Class Practice/Class Practice.cpp
@@ -0,0 +1,6 @@
+#include "Class Practice.h"
+
+char Fruit::tasteGood()
+{
+	return 0;
+}
---
+++ Stuff-in-Class/Class Practice/Class Practice/Class Practice.h
@@ -0,0 +1,17 @@
+#pragma once
+//class practice what we were doing with joining multiple together. 
+//homework make 
+class Fruit
+{
+	bool juicey;
+	bool brightcolor;
+	bool sweet;
+public:
+	char tasteGood();
+
+};
+
+class Apple
+{
+
+};
