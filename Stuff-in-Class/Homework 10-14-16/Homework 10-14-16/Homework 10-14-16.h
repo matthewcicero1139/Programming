8951716050e18477767017681cce0c3256e6fbd0
matthewcicero1139 <matthewcicero33@gmail.com>
2016-10-14 17:54:14 -0500

    Add files via upload

---
+++ Stuff-in-Class/Homework 10-14-16/Homework 10-14-16/Homework 10-14-16.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// make weapon class that takes in max damage, min damage, attack speed, damage per second
+
+/*
+	weapon
+	m_name:std:string
+	m_minimumDamage:float
+	m_maximumDamage:float
+	m_attackSpeed:float
+	DamagePerSecond():float
+*/
+
